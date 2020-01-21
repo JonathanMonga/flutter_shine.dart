705fa5fefda12b0328f471704853811dc68be5ff
Jonathan Monga <jmonga98@gmail.com>
2020-01-21 08:35:29 +0200

    Initial commit

--- packages/flutter_shine/test/flutter_shine_test.dart
+++ packages/flutter_shine/test/flutter_shine_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_shine/flutter_shine.dart';
 
 void main() {
   const MethodChannel channel = MethodChannel('flutter_shine');
